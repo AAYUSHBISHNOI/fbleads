0591ed9edb9f3e0d6d299338a0bf09eaf830994c
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2025-07-22 13:25:17 +0530

    added-env-file

--- app/utils/sheetMap.js
+++ app/utils/sheetMap.js
@@ -1,8 +1,8 @@
 // sheetMap.js
 const sheetMap = {
-  "form_id_1": "GOOGLE_SHEET_ID_1",
-  "form_id_2": "GOOGLE_SHEET_ID_2",
-  "form_id_3": "GOOGLE_SHEET_ID_3",
+  form_id_1: "1iPocy5qrKjapFKaMkgWfCmrfB2E_hQ96te2TiDY4fYw",
+  form_id_2: "GOOGLE_SHEET_ID_2",
+  form_id_3: "GOOGLE_SHEET_ID_3",
   // ...continue for each form
 };
 
